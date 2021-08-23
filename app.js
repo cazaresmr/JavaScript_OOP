a34ed3ca4f5acc044ca4954074b3c7b314fdd9f3
cazaresmr <michaelcazares@gmail.com>
2021-08-22 21:57:49 -0700

    refactored earlier attempt

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
-console.log("Hello World!\n==========\n");
+console.log("Hello World!\n==========\n")
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("EXERCISE 1:\n==========\n")
+class Person {
+	constructor(name, pets, residence, hobbies) {
+		this.name = name
+		this.pets = pets
+		this.residence = residence
+		this.hobbies = hobbies
+	}
+	info() {
+		console.log(
+			`My name is ${this.name}; I live in ${this.residence}; I have ${this.pets} pets; and my hobbies include ${this.hobbies}`
+		)
+	}
+
+greeting () {
+		console.log("Hello!");
+	}
+}
+
+class Coder extends Person {
+	constructor(name, pets, residence, hobbies) {
+		super(name, pets, residence, hobbies)
+		this.occupation = "Full Stack Web Developer"
+	}
+
+greeting() {
+	console.log("Hello World!")
+}
+}
+
+let Mike = new Person("Mike", 0, "Los Angeles", ["reading", "walking", "learning to code"])
+let Matt = new Coder("Matt", 2, "Atlanta", ["running", "watching TV", "stamp collecting"])
+
+console.log(Mike)
+console.log(Matt)
